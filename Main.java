65e670d5d0e172feadf3b16575ba4e412ff7db76
kamata keigo <k.kamata.sys23@morijyobi.ac.jp>
2024-07-12 10:47:57 +0900

    CPUの手をランダムにする処理を実装 close #16, close #17

--- Main.java
+++ Main.java
@@ -17,8 +17,23 @@ public class Main {
 
        System.out.println("あなたの手を選択してください。>");
 
+        //以降にテストコードを追加して検証を行う
 
-        
+        // System.out.println("テスト結果");
+
+        // System.out.println(user.getHand().getHandName());
+
+
+
+       //comの手を生成
+
+       com.createRandomHand();
+
+       //以降にテストコードを追加して検証を行う
+
+
+
    }
 
 }
+        
